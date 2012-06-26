57047c1cec06be50ba27421c574e5bbf99d938c1
dsaleksandrov <dprojectss@gmail.com>
2012-06-27 00:39:06 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vendetta01::Application.config.secret_token = '1ef56a3efbec03a1ceeaa8051292861e29f6c48b3598ee0f14c2bea47b636c4b00a84db583a6fb522662210886a3dd88b61c5aef959f94c9ce4badfaa11a9ef1'
